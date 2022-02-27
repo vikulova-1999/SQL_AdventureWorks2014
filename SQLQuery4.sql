2d78201ad2c81be58b3a2a3a75414740acd7b005
Taha Kula <56185668+vikulova-1999@users.noreply.github.com>
2022-02-27 15:26:24 +0300

    Add files via upload

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,18 +1,47 @@
+--1 вариант - вывод в виде таблицы
 WITH XMLNAMESPACES ('http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/Resume' AS ns)
-SELECT JobCandidateID,
+SELECT 
+JobCandidateID,
 [Resume].value('(/ns:Resume/ns:Name/ns:Name.Prefix)[1]', 'nvarchar(100)') Prefix,
 [Resume].value('(/ns:Resume/ns:Name/ns:Name.First)[1]', 'nvarchar(100)') FirstName,
 [Resume].value('(/ns:Resume/ns:Name/ns:Name.Middle)[1]', 'nvarchar(100)') Middle,
 [Resume].value('(/ns:Resume/ns:Name/ns:Name.Last)[1]', 'nvarchar(100)') LastName,
 [Resume].value('(/ns:Resume/ns:Name/ns:Name.Suffix)[1]', 'nvarchar(100)') Suffix,
-[Resume].value('(/ns:Resume/ns:Education/ns:Edu.Level)[1]', 'nvarchar(100)') EducationLevel,
-[Resume].value('(/ns:Resume/ns:Education/ns:Edu.StartDate)[1]', 'nvarchar(100)') StartDate,
-[Resume].value('(/ns:Resume/ns:Education/ns:Edu.EndDate)[1]', 'nvarchar(100)') EndDate,
-[Resume].value('(/ns:Resume/ns:Education/ns:Edu.Degree)[1]', 'nvarchar(100)') Degree,
-[Resume].value('(/ns:Resume/ns:Education/ns:Edu.School)[1]', 'nvarchar(100)') School,
-
-CASE WHEN BusinessEntityID IS NOT NULL THEN 'Да'
-ELSE 'Нет'
+y.Education.value('(ns:Edu.Level)[1]', 'nvarchar(100)') EducationLevel,
+y.Education.value('(ns:Edu.StartDate)[1]', 'nvarchar(100)') StartDate,
+y.Education.value('(ns:Edu.EndDate)[1]', 'nvarchar(100)') EndDate,
+y.Education.value('(ns:Edu.Degree)[1]', 'nvarchar(100)') Degree,
+y.Education.value('(ns:Edu.School)[1]', 'nvarchar(100)') School,
+CASE WHEN BusinessEntityID IS NOT NULL THEN 'Yes'
+ELSE 'No'
 END 'Принят на работу'
+FROM   
+    [AdventureWorks2014].[HumanResources].[JobCandidate]
+CROSS APPLY
+    [Resume].nodes('/ns:Resume/ns:Education') AS y(Education)
 
-FROM [AdventureWorks2014].[HumanResources].[JobCandidate];
+
+--2 вариант - вывод в виде XML
+/*
+WITH XMLNAMESPACES ('http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/Resume' AS ns)
+SELECT JobCandidateID, 
+[Resume].value('(/ns:Resume/ns:Name/ns:Name.Prefix)[1]', 'nvarchar(100)') Prefix,
+[Resume].value('(/ns:Resume/ns:Name/ns:Name.First)[1]', 'nvarchar(100)') FirstName,
+[Resume].value('(/ns:Resume/ns:Name/ns:Name.Middle)[1]', 'nvarchar(100)') Middle,
+[Resume].value('(/ns:Resume/ns:Name/ns:Name.Last)[1]', 'nvarchar(100)') LastName,
+[Resume].value('(/ns:Resume/ns:Name/ns:Name.Suffix)[1]', 'nvarchar(100)') Suffix,
+
+y.Education.value('(ns:Edu.Level)[1]', 'nvarchar(100)') EducationLevel,
+y.Education.value('(ns:Edu.StartDate)[1]', 'nvarchar(100)') StartDate,
+y.Education.value('(ns:Edu.EndDate)[1]', 'nvarchar(100)') EndDate,
+y.Education.value('(ns:Edu.Degree)[1]', 'nvarchar(100)') Degree,
+y.Education.value('(ns:Edu.School)[1]', 'nvarchar(100)') School,
+CASE WHEN BusinessEntityID IS NOT NULL THEN 'Yes'
+ELSE 'No'
+END 'Принят на работу'
+FROM [AdventureWorks2014].[HumanResources].[JobCandidate]
+CROSS APPLY
+[Resume].nodes('/ns:Resume/ns:Education') AS y(Education) FOR XML AUTO
+*/
+
+
